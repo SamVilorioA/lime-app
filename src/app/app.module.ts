dfacc947c395c7b6856241081bfbc10e3aa5845b
Sam Vilorio <samuelvilorio_15@hotmail.com>
2022-07-25 12:32:05 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WordWrapPipe } from './pipes/word-wrap.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WordWrapPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ItemQuantityComponent } from './components/item-quantity/item-quantity.component';
+import { SimplePageComponent } from './components/simple-page/simple-page.component';
+import { TitleComponent } from './components/title/title.component';
+import { WordWrapPipe } from './pipes/word-wrap.pipe';
+
+//angular material
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatTooltipModule} from '@angular/material/tooltip';
+
+
+
+@NgModule({
+  declarations: [ItemQuantityComponent, SimplePageComponent, TitleComponent, WordWrapPipe],
+  imports: [
+    CommonModule, MatIconModule, MatButtonModule
+  ],
+  exports: [
+    CommonModule, ItemQuantityComponent, MatButtonModule, MatIconModule, MatSnackBarModule, 
+    MatTooltipModule, SimplePageComponent, TitleComponent, WordWrapPipe
+  ]
+})
+export class SharedModule { }
