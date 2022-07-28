ca6c8713e9f97d4be6ae12bfb9817c96b715a223
Sam Vilorio <samuelvilorio_15@hotmail.com>
2022-07-28 08:59:39 -0400

    feat: finished data module

--- src/app/data/services/shipment.service.ts
+++ src/app/data/services/shipment.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorHandlerService } from 'src/app/shared/services/http-error-handler.service';
+import { Observable } from 'rxjs';
+import { Shipment } from '../models/shipment';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShipmentService {
-
-  constructor() { }
+  private url: string = `${environment.apiUrl}/api/shipments`;
+  constructor(private http: HttpClient, private eh: HttpErrorHandlerService) { }
+  getShipment(id: string): Observable<Shipment>{
+    return this.http.get<Shipment>(`${this.url}/${id}`).pipe(catchError(this.eh.handleError));
+  }
+  updateShipment(id: string, shippingMethodId: string): Observable<Shipment>{
+    return this.http.patch<Shipment>(`${this.url}/${id}`, {shippingMethodId: shippingMethodId}).pipe(catchError(this.eh.handleError));
+  }
 }
--- src/app/data/services/sku.service.ts
+++ src/app/data/services/sku.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorHandlerService } from 'src/app/shared/services/http-error-handler.service';
+import { Observable } from 'rxjs';
+import { Sku } from '../models/sku';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SkuService {
-
-  constructor() { }
+  private url: string = `${environment.apiUrl}/api/skus`;
+  constructor( private http: HttpClient, private eh: HttpErrorHandlerService) { }
+  getSku(id: string): Observable<Sku>{
+    return this.http.get<Sku>(`${this.url}/${id}`).pipe(catchError(this.eh.handleError));
+  }
+  getSkus(page: number, pageSize: number): Observable<Sku[]>{
+    return this.http.get<Sku[]>( this.url, {
+      params: {'page': page.toString(), 'pageSize': pageSize.toString()}}).pipe(catchError(this.eh.handleError));
+  }
 }
