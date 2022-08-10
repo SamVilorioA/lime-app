51241b108b9bd863a6acc53d5dbde89a3d6c604d
Sam Vilorio <samuelvilorio_15@hotmail.com>
2022-08-10 09:10:43 -0400

    feat: address and country-select components set

--- src/app/features/checkout/components/address/address.component.ts
+++ src/app/features/checkout/components/address/address.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Address } from 'src/app/data/models/address';
+import { FormGroupDirective, FormBuilder } from '@angular/forms';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-address',
@@ -6,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./address.component.css']
 })
 export class AddressComponent implements OnInit {
-
-  constructor() { }
-
+  @Input() buttonText: string = '';
+  @Input() showTitle?: boolean = false;
+  @Output() createAddress = new EventEmitter<Address>();
+  @Input() checkboxText: string = '';
+  @Output() isCheckboxChecked = new EventEmitter<boolean>();
+  countryCode: string = '';
+  addressForm = this.fb.group({
+    firstName: [''],
+    lastName: [''],
+    line1: [''],
+    city: [''],
+    zipCode: [''],
+    stateCode: [''],
+    phone: ['']
+  });
+  @ViewChild(FormGroupDirective) afDirective: FormGroupDirective | undefined;
+  constructor(private fb: FormBuilder) { }
+  setCountryCode(code: string){
+    this.countryCode = code;
+  }
+  addAddress(){
+    this.createAddress.emit({
+      firstName: this.addressForm.get('firstName')?.value,
+      lastName: this.addressForm.get('lastName')?.value,
+      line1: this.addressForm.get('line1')?.value,
+      city: this.addressForm.get('city')?.value,
+      zipCode: this.addressForm.get('zipCode')?.value,
+      stateCode: this.addressForm.get('stateCode')?.value,
+      countryCode: this.addressForm.get('countryCode')?.value,
+      phone: this.addressForm.get('phone')?.value
+    });
+  }
+  setCheckboxValue(change: MatCheckboxChange){
+    if(this.isCheckboxChecked) this.isCheckboxChecked.emit(change.checked);
+  }
   ngOnInit(): void {
   }
 
--- src/app/features/checkout/components/country-select/country-select.component.ts
+++ src/app/features/checkout/components/country-select/country-select.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { UntilDestroy } from '@ngneat/until-destroy';
+import { CountryService } from '../../../../data/services/country.service';
+import { Country } from '../../../../data/models/country';
+@UntilDestroy({ checkProperties: true})
 @Component({
   selector: 'app-country-select',
   templateUrl: './country-select.component.html',
   styleUrls: ['./country-select.component.css']
 })
 export class CountrySelectComponent implements OnInit {
-
-  constructor() { }
+  country: string = 'Country';
+  countries: Country[] = [];
+  @Output() setCountryEvent = new EventEmitter<string>();
+  constructor(private countriesServ: CountryService) { }
 
   ngOnInit(): void {
+    this.countriesServ.getCountries().subscribe(
+      countries => { this.countries = countries}
+    )
   }
-
+  setCountry(value: Country){
+    this.country = value.name || '';
+    this.setCountryEvent.emit(value.code)
+  }
 }
