189d34350d509ee740c393ff07306ab069dc75c8
Sam Vilorio <samuelvilorio_15@hotmail.com>
2022-08-11 11:34:00 -0400

    feat: address list component set

--- src/app/features/checkout/components/address-list/address-list.component.ts
+++ src/app/features/checkout/components/address-list/address-list.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { CustomerAddress } from '../../../../data/models/customer-address';
+import { SessionService } from '../../../../core/services/session.service';
+import { CustomerAddressService } from '../../../../data/services/customer-address.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { mergeMap } from 'rxjs/operators';
+import { iif } from 'rxjs';
+import { MatRadioChange } from '@angular/material/radio';
 
 @Component({
   selector: 'app-address-list',
@@ -6,10 +13,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./address-list.component.css']
 })
 export class AddressListComponent implements OnInit {
-
-  constructor() { }
+  addresses: CustomerAddress[] = [];
+  @Output() setAddressEvent = new EventEmitter<string>();
+  constructor( private session: SessionService, private customerAddresses: CustomerAddressService,
+    private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
+    this.session.loggedInStatus.pipe(mergeMap(status => iif(() => status,
+    this.customerAddresses.getCustomerAddresses()))).subscribe( addresses => {
+      if(addresses.length){
+        this.addresses = addresses
+      }
+    }, err => this.snackBar.open('There was a problem getting your existing addresses.', 'Close', {duration: 8000}))
   }
-
+  setAddress(change: MatRadioChange){
+    this.setAddressEvent.emit(change.value);
+  }
 }
