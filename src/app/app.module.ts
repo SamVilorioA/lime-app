31862e043441aa19f041735ac256b71070c921c8
Sam Vilorio <samuelvilorio_15@hotmail.com>
2022-08-14 19:45:00 -0400

    feat: modules app and checkout set

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,48 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { WordWrapPipe } from './pipes/word-wrap.pipe';
-import { ProductComponent } from './features/pages/product/product.component';
-import { ProductListComponent } from './features/pages/product-list/product-list.component';
+import { HttpClientModule, HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
+import { AuthModule } from './features/auth/auth.module';
+import { ProductsModule } from './features/products/products.module';
+import { CartModule } from './features/cart/cart.module';
+import { CheckoutModule } from './features/checkout/checkout.module';
+import { CoreModule } from './core/core.module';
+import { OptionsInterceptor } from './core/interceptors/options.interceptor';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WordWrapPipe,
-    ProductComponent,
-    ProductListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AuthModule,
+    ProductsModule,
+    CartModule,
+    CheckoutModule,
+    CoreModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: OptionsInterceptor,
+    multi: true
+  },{
+    provide: APP_INITIALIZER,
+    useFactory: (http: HttpClient) => () => http.post<object>(
+      `${environment.apiUrl}/oauth/token`,
+      {'grantType': 'client_credentials'},
+      { withCredentials: true}
+    ),
+    multi: true,
+    deps: [HttpClient]
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/features/checkout/pages/place-order/place-order.component.ts
+++ src/app/features/checkout/pages/place-order/place-order.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UntilDestroy } from '@ngneat/until-destroy';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PaypalPaymentService } from '../../../../data/services/paypal-payment.service';
+import { OrderService } from '../../../../data/services/order.service';
+import { CartService } from '../../../../data/services/cart.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UpdateOrderParams } from 'src/app/data/models/order';
+@UntilDestroy({ checkProperties: true})
 @Component({
   selector: 'app-place-order',
   templateUrl: './place-order.component.html',
   styleUrls: ['./place-order.component.css']
 })
 export class PlaceOrderComponent implements OnInit {
-
-  constructor() { }
+  disableButton = true;
+  constructor( private route: ActivatedRoute, private router: Router, private payments: PaypalPaymentService,
+    private orders: OrderService, private cart: CartService, private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
-
+  placeOrder(){
+    this.disableButton = true;
+    this.orders.updateOrder({ id: this.cart.orderId, place: true}, [UpdateOrderParams.place]).subscribe(
+      () => {
+        this.snackBar.open('Your order has been successfully placed.', 'Close', {duration: 3000});
+        this.cart.clearCart();
+        setTimeout(() => this.router.navigateByUrl('/'), 4000);
+      }, () => {
+        this.snackBar.open('There was a problem placing your order.', 'Close', { duration: 8000});
+        this.disableButton=false;
+      }
+    )
+  }
 }
