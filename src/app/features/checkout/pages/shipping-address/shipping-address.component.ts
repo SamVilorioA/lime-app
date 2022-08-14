58e48595ec4f6d13f525029bb409a3579e7ab045
Sam Vilorio <samuelvilorio_15@hotmail.com>
2022-08-13 22:42:53 -0400

    feat: billing ad, shipping ad and payment comps

--- src/app/features/checkout/pages/shipping-address/shipping-address.component.ts
+++ src/app/features/checkout/pages/shipping-address/shipping-address.component.ts
@@ -1,15 +1,89 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UntilDestroy } from '@ngneat/until-destroy';
+import { AddressService } from 'src/app/data/services/address.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SessionService } from 'src/app/core/services/session.service';
+import { OrderService } from 'src/app/data/services/order.service';
+import { CartService } from 'src/app/data/services/cart.service';
+import { Router } from '@angular/router';
+import { CustomerAddressService } from 'src/app/data/services/customer-address.service';
+import { Order, UpdateOrderParams } from 'src/app/data/models/order';
+import { Observable, iif, concat, combineLatest } from 'rxjs';
+import { concatMap } from 'rxjs/operators';
+import { Address } from 'src/app/data/models/address';
+@UntilDestroy({ checkProperties: true })
 @Component({
   selector: 'app-shipping-address',
   templateUrl: './shipping-address.component.html',
   styleUrls: ['./shipping-address.component.css']
 })
 export class ShippingAddressComponent implements OnInit {
-
-  constructor() { }
+  showAddresses: boolean = false;
+  sameBillingAddressAsShipping: boolean = false;
+  selectedCustomerAddressId: string = '';
+  constructor( private addresses: AddressService, private snackBar: MatSnackBar,
+    private session: SessionService, private orders: OrderService, private cart: CartService,
+    private router: Router, private customerAddresses: CustomerAddressService) { }
 
   ngOnInit(): void {
+    this.session.loggedInStatus.subscribe( status => this.showAddresses = status)
   }
-
+  updateShippingAddress(address: Address){
+    if(this.showAddresses && this.selectedCustomerAddressId){
+      this.cloneAddress();
+    }else if( address.firstName && address.lastName && address.line1 && address.city
+      && address.zipCode && address.stateCode && address.countryCode && address.phone){
+        this.createAddress(address)
+      }else{
+        this.snackBar.open('Check your address. Some fields are missing.', 'Close');
+      }
+  }
+  setCustomerAddress(customerAddressId: string){
+    this.selectedCustomerAddressId = customerAddressId;
+  }
+  setSameBillingAddressAsShipping(change: boolean){
+    this.sameBillingAddressAsShipping=change;
+  }
+  private createAddress( address: Address){
+    this.addresses.createAddress(address).pipe(
+      concatMap( address => {
+        const update = this.updateOrderObservable({ id: this.cart.orderId, shippingAddressId: address.id},
+          [UpdateOrderParams.shippingAddress]);
+        if( this.showAddresses){
+          return combineLatest([ update, this.customerAddresses.createCustomerAddress(address.id || '', '')]);
+        }else {
+          return update;
+        }
+      })
+    ).subscribe(
+      () => this.showSuccessSnackBar(),
+      err => this.showErrorSnackBar()
+    )
+  }
+  private cloneAddress(){
+    this.updateOrderObservable({
+      id: this.cart.orderId,
+      shippingAddressCloneId: this.selectedCustomerAddressId}, [UpdateOrderParams.shippingAddressClone]
+      ).subscribe(
+        () => this.showSuccessSnackBar(),
+        err => this.showErrorSnackBar()
+      );
+  }
+  private updateOrderObservable(order: Order, updateParams: UpdateOrderParams[]): Observable<any>{
+    return iif(() => this.sameBillingAddressAsShipping,
+    concat([
+      this.orders.updateOrder(order, updateParams),
+      this.orders.updateOrder(order, [UpdateOrderParams.billingAddressSameAsShipping])
+    ]),
+    this.orders.updateOrder(order, updateParams))
+  }
+  private showErrorSnackBar(){
+    this.snackBar.open('There was a problem creating your address.', 'Close', {duration: 8000});
+  }
+  private showSuccessSnackBar(){
+    this.snackBar.open('Shipping address successfully added. Redicting...', 'Close', {duration: 3000});
+    setTimeout(() => {
+      this.router.navigateByUrl('/shipping-methods')
+    }, 4000);
+  }
 }
--- src/app/features/checkout/pages/shipping-methods/shipping-methods.component.ts
+++ src/app/features/checkout/pages/shipping-methods/shipping-methods.component.ts
@@ -1,15 +1,91 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { UntilDestroy } from '@ngneat/until-destroy';
+import { Shipment } from 'src/app/data/models/shipment';
+import { DeliveryLeadTimeService } from 'src/app/data/services/delivery-lead-time.service';
+import { OrderService } from 'src/app/data/services/order.service';
+import { CartService } from 'src/app/data/services/cart.service';
+import { Router } from '@angular/router';
+import { ShipmentService } from 'src/app/data/services/shipment.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { DeliveryLeadTime } from 'src/app/data/models/delivery-lead-time';
+import { ShippingMethod } from 'src/app/data/models/shipping-method';
+import { LineItem } from 'src/app/data/models/line-item';
+import { combineLatest } from 'rxjs';
+import { GetOrderParams } from 'src/app/data/models/order';
+@UntilDestroy({ checkProperties: true})
 @Component({
   selector: 'app-shipping-methods',
   templateUrl: './shipping-methods.component.html',
   styleUrls: ['./shipping-methods.component.css']
 })
 export class ShippingMethodsComponent implements OnInit {
-
-  constructor() { }
+  shipments: Shipment[] | undefined = [];
+  shipmentsForm: FormGroup = this.fb.group({});
+  constructor( private orders: OrderService, private dlts: DeliveryLeadTimeService,
+    private cart: CartService, private router: Router, private fb: FormBuilder,
+    private shipmentServ: ShipmentService, private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
-  }
+    combineLatest([
+      this.orders.getOrder(this.cart.orderId, GetOrderParams.shipments),
+      this.dlts.getDeliveryLeadTimes()
+    ]).subscribe(
+      values => {
+        const lineItems = values[0].lineItems || [];
+        const deliveryLeadTimes = values[1] || [];
+        let li: LineItem;
+        let lt: DeliveryLeadTime[];
 
+        this.shipments = values[0].shipments?.map((shipment) => {
+          if (shipment.id) {
+            this.shipmentsForm.addControl(shipment.id, new FormControl('', Validators.required));
+          }
+
+          if (shipment.lineItems) {
+            shipment.lineItems = shipment.lineItems.map(item => {
+              li = this.findItem(lineItems, item.skuCode || '');
+              item.imageUrl = li.imageUrl;
+              item.name = li.name;
+              return item;
+            });
+          }
+
+          if (shipment.availableShippingMethods) {
+            lt = this.findLocationLeadTime(deliveryLeadTimes, shipment);
+            shipment.availableShippingMethods = shipment.availableShippingMethods?.map(
+              method => {
+                method.deliveryLeadTime = this.findMethodLeadTime(lt, method);
+                return method;
+              });
+          }
+
+          return shipment;
+        });
+      },
+      err => this.router.navigateByUrl('/error')
+    );
+  }
+  setShipmentMethods() {
+    const shipmentsFormValue = this.shipmentsForm.value;
+
+    combineLatest(Object.keys(shipmentsFormValue).map(
+      key => this.shipmentServ.updateShipment(key, shipmentsFormValue[key])
+    )).subscribe(
+      () => {
+        this.snackBar.open('Your shipments have been updated with a shipping method.', 'Close', { duration: 3000 });
+        setTimeout(() => this.router.navigateByUrl('/payment'), 4000);
+      },
+      err => this.snackBar.open('There was a problem adding shipping methods to your sipments.', 'Close', { duration: 5000 })
+    );
+  }
+  private findItem(lineItems: LineItem[], skuCode: string): LineItem{
+    return lineItems.filter((item) => item.skuCode == skuCode)[0];
+  }
+  private findLocationLeadTime(times: DeliveryLeadTime[], shipment: Shipment): DeliveryLeadTime[]{
+    return times.filter((dlTime) => dlTime?.stockLocation?.id == shipment?.stockLocation?.id)
+  }
+  private findMethodLeadTime(times: DeliveryLeadTime[], method: ShippingMethod): DeliveryLeadTime{
+    return times.filter((dlTime) => dlTime?.shippingMethod?.id == method.id)[0];
+  }
 }
